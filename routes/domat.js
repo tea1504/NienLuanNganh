3d77b3de4353e0607960b0687aa422272f75b5f9
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 14:37:40 +0700

    PUT method

--- routes/domat.js
+++ routes/domat.js
@@ -14,4 +14,66 @@ router.get('/', (req, res, next) => {
     .catch(err => {
       res.status(500).json(err);
     })
-})
+});
+
+/**
+ * GET /domat/:id
+ * Lấy 1 document trong collection domat
+ * @param {IdObject} id - ID của document
+ */
+router.get("/:id", (req, res, next) => {
+  var id = req.params.id;
+
+  doMatModel.findById(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+/**
+ * POST /domat
+ * Thêm mới 1 document vào collection domat
+ * @param {String} ten - tên của độ mật
+ */
+router.post('/', (req, res, next) => {
+  var ten = req.body.ten;
+
+  doMatModel.create({
+    ten: ten,
+  })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+/**
+ * PUT /domat/:id
+ * Cập nhật document trong collection domat theo id
+ * @param {IdObject} id - ID của độ mật
+ * @param {String} ten - Tên của độ mật
+ */
+router.put('/:id', (req, res, next) => {
+  var id = req.params.id;
+  var ten = req.body.ten;
+
+  doMatModel.findByIdAndUpdate(id, {
+    ten: ten,
+  })
+    .then(data => {
+      return doMatModel.findById(data.id);
+    })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+module.exports = router;
