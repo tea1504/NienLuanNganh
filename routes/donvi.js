02b91e431a28e9c179e009e778d834a78dd95fd8
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 20:36:18 +0700

    GET (all) method

--- routes/donvi.js
+++ routes/donvi.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 var donViModel = require('../model/donvi');
 
-
+/**
+ * GET /donvi
+ * Lấy toàn bộ dữ liệu trong collection donvi 
+ */
+router.get('/', (req, res, next) => {
+  donViModel.find({})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 module.exports = router;
