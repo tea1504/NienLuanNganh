1ad0eb088a88bc15a5068c39393628274f29631f
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 14:59:32 +0700

    GET (all) method

--- routes/trangthai.js
+++ routes/trangthai.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var doMatModel = require('../model/domat');
+var trangThaiModel = require('../model/trangthai');
+
+/**
+ * GET /trangthai
+ * Lấy toàn bộ dữ liệu trong collection trangthai 
+ */
+router.get('/', (req, res, next) => {
+  trangThaiModel.find({})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 module.exports = router;
