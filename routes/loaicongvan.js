7b230f46ab38c0be2337c44d31ad15fe6be52a0d
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 15:38:10 +0700

    GET (all) method

--- routes/loaicongvan.js
+++ routes/loaicongvan.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 var loaiCongVanModel = require('../model/loaicongvan');
 
-
+/**
+ * GET /loaicongvan
+ * Lấy toàn bộ dữ liệu trong collection loaicongvan 
+ */
+router.get('/', (req, res, next) => {
+  loaiCongVanModel.find({})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 module.exports = router;
