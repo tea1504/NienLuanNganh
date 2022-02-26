8b432efa44a1093a53634285347447e2a1d319ba
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 20:23:34 +0700

    donvi.js

---
+++ model/donvi.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+var mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var Schema = mongoose.Schema;
+
+var DonViSchema = new Schema({
+  ten: {
+    type: String,
+    required: [true, 'Bạn phải nhập tên độ mật'],
+  },
+  benngoai: {
+    type: Boolean,
+    default: false,
+  },
+  email: {
+    type: String,
+    required: [true, 'Bạn phải nhập địa chỉ email cho đơn vị'],
+    validate: {
+      validator: function(v) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  }
+});
+
+var DonViModel = mongoose.model("DonVi", DonViSchema);
+
+module.exports = DonViModel;
