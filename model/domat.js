12214d0c9301b82058a44400f1e90b0989b35737
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 14:05:30 +0700

    domat.js

--- model/domat.js
+++ model/domat.js
@@ -1,1 +1,20 @@
+require("dotenv").config();
+var mongoose = require("mongoose");
 
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var Schema = mongoose.Schema;
+
+var DoMatSchema = new Schema({
+  ten: {
+    type: String,
+    required: [true, 'Bạn phải nhập tên'],
+  },
+});
+
+var DoMatModel = mongoose.model("DoMat", DoMatSchema);
+
+module.exports = DoMatModel;
