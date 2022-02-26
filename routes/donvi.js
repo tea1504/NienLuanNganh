24176de60c2dffe697f4bc50d5454b02566bc0ce
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-02-26 20:44:36 +0700

    POST method

--- routes/donvi.js
+++ routes/donvi.js
@@ -33,4 +33,33 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
+/**
+ * POST /donvi
+ * Thêm mới 1 document vào collection donvi
+ * @param {String} ten - tên của đơn vị
+ * @param {String/Boolean} benngoai - đơn vị có phải bên ngoài hệ thống không
+ * @param {String} ten - tên của đơn vị
+ */
+router.post('/', (req, res, next) => {
+  var ten = req.body.ten;
+  var benngoai;
+  if(typeof req.body.benngoai === 'string')
+    benngoai = (req.body.benngoai === 'true');
+  else
+    benngoai = req.body.benngoai;
+  var email = req.body.email;
+
+  donViModel.create({
+    ten: ten,
+    benngoai: benngoai,
+    email: email,
+  })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
 module.exports = router;
