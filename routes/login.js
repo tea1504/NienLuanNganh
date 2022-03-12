da5abe6fb1d758966bd1da6dab70fda8fbb705ba
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-03-12 16:54:13 +0700

    check user actived

--- routes/login.js
+++ routes/login.js
@@ -13,13 +13,18 @@ router.post('/', (req, res, next) => {
   })
     .then(data => {
       if (data.comparePassword(matkhau)) {
-        var token = jwt.sign({ ma: data.ma }, process.env.PRIVATEKEY, { expiresIn: '2h' });
-        res.cookie("jwt", token, {
-          //expires: new Date(Date.now),
-          maxAge: 2 * 60 * 60 * 1000,
-          httpOnly: true,
-        });
-        res.send(token);
+        if (data.actived) {
+          var token = jwt.sign({ ma: data.ma }, process.env.PRIVATEKEY, { expiresIn: '2h' });
+          res.cookie("jwt", token, {
+            //expires: new Date(Date.now),
+            maxAge: 2 * 60 * 60 * 1000,
+            httpOnly: true,
+          });
+          res.send(token);
+        }
+        else {
+          res.status(403).send("Tài khoản bị khóa");
+        }
       }
       else
         res.status(403).send("Đăng nhập sai");
